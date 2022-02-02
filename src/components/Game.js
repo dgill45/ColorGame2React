ed3ed9ffa3fbe0f50f6fc257db73b89b81122438
donellgill <donell.gill@outlook.com>
2022-02-02 16:50:33 -0500

    created Game component with h1

---
+++ src/components/Game.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Game(){
+
+    return (
+
+        <div className ='Game'>
+            <h1>The Great
+                <br />
+                <span id ="colorDisplay">Mahlia</span>
+                <br />
+	            Color Game
+            </h1>
+            <h4>Choose the color:</h4>
+        </div>
+    )
+}
+
+export default Game;
